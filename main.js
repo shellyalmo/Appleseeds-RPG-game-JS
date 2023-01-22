b5a6ecfe55164ea1604d53ef66da2baf51289591
shellyalmo <shellyalmo@gmail.com>
2023-01-22 13:08:30 +0200

    feat: add player initialization

--- main.js
+++ main.js
@@ -108,3 +108,20 @@ let monstersInEachLocation = mapLocationMonsters(monsters, finalLocations);
 // }
 
 // console.log(listLocationsContracts(contracts, monstersInEachLocation));
+
+/*player */
+function createPlayer() {
+  let playerName = prompt("Enter player name: ");
+  let playerObj = {};
+  playerObj.playerName = playerName;
+  playerObj.strength = Math.round(Math.random() * (100 - 1) + 1);
+  playerObj.defense = Math.round(Math.random() * (100 - 1) + 1);
+  playerObj.dexterity = Math.round(Math.random() * (10 - 1) + 1);
+  playerObj.goldAmount = 100;
+  playerObj.potionAmount = 3;
+  playerObj.level = 1;
+  playerObj.healthPoints = 100;
+  playerObj.currentXP = 0;
+}
+
+let player = createPlayer();
